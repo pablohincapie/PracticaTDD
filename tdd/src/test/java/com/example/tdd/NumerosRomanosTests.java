0839f158d33123efc131c76b608f1a1473d50e9a
Pablo Hincapie <pabloandres.hincapie@woloc.co>
2021-05-09 21:34:08 -0500

    Cambios en feature 2

--- tdd/src/test/java/com/example/tdd/NumerosRomanosTests.java
+++ tdd/src/test/java/com/example/tdd/NumerosRomanosTests.java
@@ -1,5 +1,10 @@
 package com.example.tdd;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import javax.swing.ListCellRenderer;
+import javax.swing.ListModel;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
@@ -50,6 +55,107 @@ public class NumerosRomanosTests {
 	public void pasar9Aromanos() {
 		String romano = numerosRomanos.convertirAromanos(9);
 		assertEquals(romano, "IX");
+	}
+	@Test
+	public void pasar10Aromanos() {
+		String romano = numerosRomanos.convertirAromanos(10);
+		assertEquals(romano, "X");
+	}
+	@Test
+	public void pasar11Aromanos() {
+		String romano = numerosRomanos.convertirAromanos(11);
+		assertEquals(romano, "XI");
+	}
+	@Test
+	public void pasar12Aromanos() {
+		String romano = numerosRomanos.convertirAromanos(12);
+		assertEquals(romano, "XII");
+	}
+	@Test
+	public void pasar13Aromanos() {
+		String romano = numerosRomanos.convertirAromanos(13);
+		assertEquals(romano, "XIII");
+	}
+	@Test
+	public void pasar14Aromanos() {
+		String romano = numerosRomanos.convertirAromanos(14);
+		assertEquals(romano, "XIV");
+	}
+	@Test
+	public void pasar15Aromanos() {
+		String romano = numerosRomanos.convertirAromanos(15);
+		assertEquals(romano, "XV");
+	}
+	@Test
+	public void pasar16Aromanos() {
+		String romano = numerosRomanos.convertirAromanos(16);
+		assertEquals(romano, "XVI");
+	}
+	@Test
+	public void pasar17Aromanos() {
+		String romano = numerosRomanos.convertirAromanos(17);
+		assertEquals(romano, "XVII");
+	}
+	@Test
+	public void pasar18Aromanos() {
+		String romano = numerosRomanos.convertirAromanos(18);
+		assertEquals(romano, "XVIII");
+	}
+	@Test
+	public void pasar19Aromanos() {
+		String romano = numerosRomanos.convertirAromanos(19);
+		assertEquals(romano, "XIX");
+	}
+	@Test
+	public void pasar20Aromanos() {
+		String romano = numerosRomanos.convertirAromanos(20);
+		assertEquals(romano, "XX");
+	}
+	@Test
+	public void verificarVeintenaNumeroRomanos(){
+		List<Integer> numerosNaturales = Arrays.asList(20,21,22,23,24,25,26,27,28,29);
+		List<String> romanos = numerosNaturales.stream().map(number -> numerosRomanos.convertirAromanos(number)).collect(
+				Collectors.toList());
+		assertEquals(romanos.get(0), "XX");
+		assertEquals(romanos.get(1), "XXI");
+		assertEquals(romanos.get(2), "XXII");
+		assertEquals(romanos.get(3), "XXIII");
+		assertEquals(romanos.get(4), "XXIV");
+		assertEquals(romanos.get(5), "XXV");
+		assertEquals(romanos.get(6), "XXVI");
+		assertEquals(romanos.get(7), "XXVII");
+		assertEquals(romanos.get(8), "XXVIII");
+		assertEquals(romanos.get(9), "XXIX");
+	}
+	@Test
+	public void verificarTreintenaNumeroRomanos(){
+		List<Integer> numerosNaturales = Arrays.asList(30,31,32,33,34,35,36,37,38,39);
+		List<String> romanos = numerosNaturales.stream().map(number -> numerosRomanos.convertirAromanos(number)).collect(
+				Collectors.toList());
+		assertEquals(romanos.get(0), "XXX");
+		assertEquals(romanos.get(1), "XXXI");
+		assertEquals(romanos.get(2), "XXXII");
+		assertEquals(romanos.get(3), "XXXIII");
+		assertEquals(romanos.get(4), "XXXIV");
+		assertEquals(romanos.get(5), "XXXV");
+		assertEquals(romanos.get(6), "XXXVI");
+		assertEquals(romanos.get(7), "XXXVII");
+		assertEquals(romanos.get(8), "XXXVIII");
+		assertEquals(romanos.get(9), "XXXIX");
+	}
+	@Test
+	public void verificarDecimalesRomanos(){
+		List<Integer> numerosNaturales = Arrays.asList(40,50,60,70,80,90,88, 67);
+		List<String> romanos = numerosNaturales.stream().map(number -> numerosRomanos.convertirAromanos(number)).collect(
+				Collectors.toList());
+		assertEquals(romanos.get(0), "XL");
+		assertEquals(romanos.get(1), "L");
+		assertEquals(romanos.get(2), "LX");
+		assertEquals(romanos.get(3), "LXX");
+		assertEquals(romanos.get(4), "LXXX");
+		assertEquals(romanos.get(5), "XC");
+		assertEquals(romanos.get(6), "LXXXVIII");
+		assertEquals(romanos.get(7), "LXVII");
 	}
 
 }
